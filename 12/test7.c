2b8d35c444019904c6caf780d009da76652d0eeb
mumu3w <mumu3w@outlook.com>
2015-11-01 09:55:12 +0800

    add 12

---
+++ 12/test7.c
@@ -0,0 +1,49 @@
+﻿/******************************************************************************
+  题目: (练习题14)
+		
+  演示: 
+		
+  邮箱：mumu3w@outlook.com	
+  
+  时间：10/31/2015 
+  
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DAYS	7
+#define HOURS	24
+
+#define TRUE	1
+#define FALSE	0
+typedef unsigned BOOL;
+
+BOOL search(const int a[], int n, int key);
+
+int main(void){
+	int i;
+	int temperatures[DAYS][HOURS] = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 32}};
+	
+	for(i = 0; i < DAYS; i++){
+		printf("\n%d\n", search(temperatures[i], HOURS, 32));
+	}
+	
+	return 0;
+}
+
+BOOL search(const int a[], int n, int key){
+	int i;
+	BOOL mark = FALSE;
+
+	i = 0;
+	while(i < n){
+		if(*(a+i) == key){
+			mark = TRUE;
+		}
+		printf(" %d", *(a+i));
+		i++;
+	}
+	
+	return mark;
+}
---
+++ 12/test8.c
@@ -0,0 +1,43 @@
+﻿/******************************************************************************
+  题目: (练习题16)
+		
+  演示: 
+		
+  邮箱：mumu3w@outlook.com	
+  
+  时间：10/31/2015 
+  
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DAYS	7
+#define HOURS	24
+
+int find_largest(int a[], int n);
+
+int main(void){
+	int i;
+	int temperatures[DAYS][HOURS] = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 32}};
+	
+	for(i = 0; i < DAYS; i++){
+		printf("max:%-3d\n", find_largest(temperatures[i], HOURS));
+	}
+	
+	return 0;
+}
+
+int find_largest(int a[], int n){
+	int *p = a, max;
+	
+	max = *p;
+	while(p < a+n){
+		if(*p > max){
+			max = *p;
+		}
+		p++;
+	}
+	
+	return max;
+}
