c1cdf6928485f6301a2f9c2237d798e80e4a6ad4
mumu3w <mumu3w@outlook.com>
2015-11-30 14:22:28 +0800

    Add the first chapter

---
+++ B/lib/myc.h
@@ -0,0 +1,16 @@
+﻿#ifndef __MYC_H__
+#define __MYC_H__
+
+
+
+#ifndef TRUE
+typedef int BOOL;
+#define TRUE (1 == 1)
+#define FALSE (0 == 1)
+#endif
+
+extern void Error(const char *format, ...);
+extern int read_line(char buffer[], int n, FILE **in_fp);
+
+
+#endif
