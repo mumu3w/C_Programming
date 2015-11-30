5deead1e27945f5b1bab8164a3416e9420f180e2
mumu3w <mumu3w@outlook.com>
2015-11-30 12:31:40 +0800

    A and B

--- 15/15.1/word.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef	WORD_H
-#define WORD_H
-
-void read_word(char *word, int len);
-
-#endif
--- 15/15.4/read_line.h
+++
@@ -1,6 +0,0 @@
-﻿#ifndef READ_LINE_H
-#define READ_LINE_H
-
-int read_line(char str1[], int n);
-
-#endif
